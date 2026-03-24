f3087a4e9ebaed4ad674b2c048e0788f6674a549
Michael B <m.belousov@pugofka.com>
2026-03-24 21:04:50 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { config as loadEnv } from 'dotenv';
+
+loadEnv();
+
+const app = express();
+
+const mongoUri = process.env.MONGO_URI;
+const host = process.env.HOST || 'http://localhost';
+const port = process.env.PORT || 3333;
+
+mongoose
+    .connect(mongoUri)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log('Failed to connect to MongoDB', err);
+    })
+    ;
+
+app.listen(port, () => {
+    console.log(`Server is running on ${host}:${port}`);
+});
+
+app.use(express.json());
