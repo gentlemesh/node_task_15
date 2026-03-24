71c17ac0027630eb55a581239da1e7299cecfab7
Michael B <m.belousov@pugofka.com>
2026-03-24 23:10:50 +0100

    product: fetch all with filter

--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,34 @@ import Product from '../models/Product.js';
 
 const router = Router();
 
+router.get('/', async (req, res) => {
+    const { category, sortby, sort = 'asc' } = req.query || {};
+    if (sortby && !Object.getOwnPropertyNames(Product.schema.obj).includes(sortby)) {
+        return res.status(422).json({ message: 'Cannot sort by a non-existent field' });
+    }
+    if (sort && !['asc', 'desc'].includes(sort)) {
+        return res.status(422).json({ message: 'Incorrect sort value' });
+    }
+
+    const filter = {};
+    if (category) {
+        filter.category = category;
+    }
+
+    const query = Product.find(filter);
+    if (sortby) {
+        query.sort({ [sortby]: sort });
+    }
+
+    try {
+        const products = await query.exec();
+
+        return res.json({ products });
+    } catch (err) {
+        return res.status(500).json({ message: 'Failed to get products' });
+    }
+});
+
 router.post('/', async (req, res) => {
     const { name, price, category } = req.body || {};
     if (!name) {
